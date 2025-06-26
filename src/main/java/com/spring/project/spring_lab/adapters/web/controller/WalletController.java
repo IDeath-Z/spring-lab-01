7c57fdde895a3242a14b52d1a0bb9f32383314f3
DeathZ <jose.joz46@hotmail.com>
2025-06-25 21:10:42 -0300

    Remoção da funcionalidade de alteração de carteira padrão

--- src/main/java/com/spring/project/spring_lab/adapters/web/controller/WalletController.java
+++ src/main/java/com/spring/project/spring_lab/adapters/web/controller/WalletController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,12 +27,6 @@ public class WalletController {
     public ResponseEntity<?> createNewWallet(@PathVariable UUID accountId) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(walletService.addWallet(accountId));
-    }
-
-    @PatchMapping("/set-default/{walletId}")
-    public ResponseEntity<?> setDefaultWallet(@PathVariable UUID walletId) {
-
-        return ResponseEntity.status(HttpStatus.OK).body(walletService.switchDefaultWallet(walletId));
     }
 
     @GetMapping("/{walletId}")
